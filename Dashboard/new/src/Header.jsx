12f31285dcc725901fac47b013bd7359348eab51
vishnu11N <108918646+vishnu11N@users.noreply.github.com>
2023-10-17 15:52:44 +0530

    Add files via upload

---
+++ Dashboard/new/src/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
+
+function Header({ OpenSidebar }) {
+  return (
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={OpenSidebar} />
+      </div>
+      <div className="header-left">HELLO VISHNU👋</div>
+      <div className="header-right">
+        <BsSearch className="icon"></BsSearch>
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
